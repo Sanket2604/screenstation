49bd456c969ab575f33cae54122421c6e050a149
Sanket2604 <umaymailme2604@gmail.com>
2021-01-10 23:05:01 +0530

    Front end final

--- js/indexowl.js
+++ js/indexowl.js
@@ -20,26 +20,5 @@ $(document).ready(function(){
             }
         }
     });
-    $('.owl-carousel2').owlCarousel({
-        autoplay:false,
-        nav: true,
-        loop:false,
-        navText : ["<div class='prev-slide'><i class='fa fa-chevron-left'></i></div>",
-        "<div class='next-slide'><i class='fa fa-chevron-right'></i></div>"],
-        smartSpeed:1000,
-        slideTransition:'linear',
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            760:{
-                items:2,
-            },
-            1190:{
-                items:3,
-            }
-        }
-    });
 });
 
